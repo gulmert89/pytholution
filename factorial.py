ff516438d2fb75986da38589b05d6071123229ad
Mert Gül <gul.mert89@gmail.com>
2020-02-24 16:01:41 +0300

    cosmetic changes

--- factorial.py
+++ factorial.py
@@ -1,5 +1,6 @@
-# Factorial of which number we would like to calculate? Let's take an input first:
-x=int(input("Enter the integer of which the factorial is to be calculated: "))
+# Factorial of which number we would like to calculate? #
+
+x = int(input("Enter the integer of which the factorial is to be calculated: "))
 
 def fact(x):
     if x==0:    #you know, factorial of 0 is 1
@@ -10,4 +11,5 @@ if x>=0:
     print("The factorial of",x,"is",fact(x),end=".")
 else:
     print("Please avoid negative numbers.")
-    # raise ValueError("Please avoid negative numbers.")    #this is an alternative answer to entering a negative number but i just like to see a simple string instead of a scary "ValueError!!"
+    # raise ValueError("Please avoid negative numbers.")
+    # ^this is an alternative answer to entering a negative number but i just like to see a simple string instead of a scary "ValueError!!"
