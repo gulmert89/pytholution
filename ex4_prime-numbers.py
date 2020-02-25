a9e499d5ef54c20a5c3a9921b253f56b7b6629a4
Mert Gül <gul.mert89@gmail.com>
2020-02-25 16:11:22 +0300

    Update ex4_prime-numbers.py

--- ex4_prime-numbers.py
+++ ex4_prime-numbers.py
@@ -12,8 +12,12 @@ for i in range(1,ep):
         
 print("List of the prime numbers up to {} is:\n{}".format(ep,pl))
 
-# Note to myself: The meaning of "else" aligned with the "for" above: If none of "if" values are satisfied for all the "for" values, do the "else".
+# Note to myself: The meaning of "else" aligned with the "for" above:
+    # If none of "if" values are satisfied for all the "for" values, do the "else".
 # Loved this usage of "else".
 # I was a bit reluctant to use .format() while string formatting but well, it seems quite fun.
 # It was quite hard to imagine how to create a "for" under a "for" to dig out the prime numbers but a simple pencil and paper helped me.
-# I know, it's a simple code but still, I -somehow- need to make the code simpler because it divides the entire series over and over again. There was this thing about "Fermat's theorem" that states that you just need to divide the number up to its square root but I don't remember what it was exactly.).
+# I know it's a simple code but still, I -somehow- need to make the code simpler
+    # because it divides the entire range() over and over again.
+    # There was this thing about "Fermat's theorem" that states that you just need to
+    # divide the number up to its square root but I don't remember what it was exactly.
