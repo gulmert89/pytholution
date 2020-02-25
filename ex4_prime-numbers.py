19df9e31241b02072dbbe4d81b3a6b11919728c3
Mert Gül <gul.mert89@gmail.com>
2020-02-25 14:44:37 +0200

    Update ex4_prime-numbers.py

--- ex4_prime-numbers.py
+++ ex4_prime-numbers.py
@@ -10,7 +10,10 @@ for i in range(1,ep):
     else:
         pl.append(i)
         
-print("List of the prime numbers up to",ep,"is:\n",pl)
+print("List of the prime numbers up to {} is:\n{}".format(ep,pl))
 
 # Note to myself: The meaning of "else" aligned with the "for" above: If none of "if" values are satisfied for all the "for" values, do the "else".
 # Loved this usage of "else".
+# I was a bit reluctant to use .format() while string formatting but well, it seems quite fun.
+# It was quite hard to imagine how to create a "for" under a "for" to dig out the prime numbers but a simple pencil and paper helped me.
+# I know, it's a simple code but still, I -somehow- need to make the code simpler because it divides the entire series over and over again. There was this thing about "Fermat's theorem" that states that you just need to divide the number up to its square root but I don't remember what was it exactly.).
