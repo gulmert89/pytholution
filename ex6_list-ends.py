7090d6c08e2849a994c161be7d78f9a396af472c
Mert Gül <gul.mert89@gmail.com>
2020-02-28 22:42:03 +0300

    made the code leaner
    changed the code a bit to make it more readable. also discovered a great usage of map()

--- ex6_list-ends.py
+++ ex6_list-ends.py
@@ -4,17 +4,15 @@
 # The question is: Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) and makes a new list of only the first and last elements of the given list. For practice, write this code inside a function.
 
 def myList(elements):
-    list1 = elements.split(" ")    # let me sort our your input first, okay?
-    list2 = []
-    list2.append(list1[0])    # 
-    list2.append(list1[-1])
+    list1 = elements.split(" ")    # split() already gives me a list. Nice!
+    list2 = [list1[0], list1[-1]]    # but the elements are not integers. they are still strings now:( I don't like it.
+    list2 = list(map(int, list2))    # ask Google the right questions and the answers shall appear. Now we have integers!
     print(list2)
     
 yourInput= input("Enter a list of numbers divided by space: ")
 myList(yourInput)
 
-# For some time now, I was wondering how one can give multiple inputs divided by space, i.e 1 3 12 150 42.
-    # I came up with .split() module and it fit to this problem. Maybe there exists a different input() for that. We will see.
-# Also I was trying to execute my "example.py" file on my python shell and I've just learned how to do it:
-    # Here the command is: exec(open("C:\\Users\\mycomputer\\Desktop\\example.py").read())
-    # I studied how to write/read/edit etc. files a month ago but I need to do more exercises on it, obviously.
+# For some time now, I was wondering how one can give multiple inputs divided by spaces, i.e 1 3 12 150 42. I came up with .split() module to do that and it fit to this problem nice. Maybe there exists a different input() for providing multiple inputs. I won't use my Googling skills at once.
+# Note to myself:
+# 1) I know what map() does but knowing something is different than internalising it. That was an exciting usage of map()!
+# 2) I saw a few tricks to reduce the number of lines but as it's said: "Readability counts."
