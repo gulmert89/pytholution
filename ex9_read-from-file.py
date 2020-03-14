015a3f9b56cb6d05580b758a017104487ff1a003
Mert Gül <gul.mert89@gmail.com>
2020-03-14 15:29:21 +0300

    End of the coding part
    I'll fix some comments later. Ah, it's working now at last!

--- ex9_read-from-file.py
+++ ex9_read-from-file.py
@@ -1,34 +1,39 @@
-import urllib.request as url
+### Read Me Good ###
 
-ppText = url.urlopen("http://www.practicepython.org/assets/nameslist.txt").read()
-ppText = ppText.decode("utf-8")
+import urllib.request as url
+# 
 
 # my solution for Practice Python: Exercise 22
 def sol():
     print("--- Solution 1 ---")
+
+    ppText = url.urlopen("http://www.practicepython.org/assets/nameslist.txt").read()
+    ppText = ppText.decode("utf-8")
     ppList = ppText.split()
     ppDict = {}
+
     for i in ("Darth","Lea","Luke"):
         ppDict[i] = ppList.count(i)
+
     print(ppDict)
-    print("Total number of names is:",len(ppList))
+    print("Total number of names is:",len(ppList), end="\n")
 
 # my solution for the "Extra" part of the exercise
 def sol_ext():
-    print("--- Extra Solution ---")
+    print("\n--- Extra Solution ---")
+
     ppText_ext = url.urlopen("http://www.practicepython.org/assets/Training_01.txt").read()
     ppText_ext = ppText_ext.decode("utf-8")
     ppList_ext = ppText_ext.split()
-    ppDict_ext = {}
-    ppCategory = set()
-    for i in ppList_ext:    # YOU ARE CURRENTLY HERE. RETURN HERE TOMORROW
-        ppCategory.add(ppList_ext[3:ppList_ext.index("/",3,(len(ppList_ext)-1))])
-    for i in ("Darth","Lea","Luke"):
-        ppDict[i] = ppList.count(i)
-    print(ppDict)
-    print("Total number of names is:",len(ppList))
+    ppSet_category = set()
 
+    for i in range(len(ppList_ext)):
+        find_NextSlash = ppList_ext[i].index("/",3,(len(ppList_ext)-1))
+        each_category = ppList_ext[i][3:find_NextSlash]
+        ppSet_category.add(each_category)
 
+    print("The total number of images is:", len(ppList_ext))
+    print("The total number of categories is:", len(ppSet_category), end="\n")
 
     # you can use these:
     # str.index(sub, start, end)
@@ -36,7 +41,7 @@ def sol_ext():
     # the idea: take the word between the two indices of two /forward slashes/
 
 
-sol(); print("\n")
+sol()
 sol_ext()
 
 # reference webpages: https://stackoverflow.com/questions/1393324/in-python-given-a-url-to-a-text-file-what-is-the-simplest-way-to-read-the-cont
